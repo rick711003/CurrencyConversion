5b7d55c4772f8c2c01de79ebdb981c9c8d3da64c
Wen Chien Chen <rick711003@gmail.com>
2021-01-17 18:35:43 +0800

    Add Cell Snapshot test case, full data case and nil data case

--- CurrencyConversionTests/SnapshotTests/ExchangeRateCellSnapshotTests.swift
+++ CurrencyConversionTests/SnapshotTests/ExchangeRateCellSnapshotTests.swift
@@ -5,4 +5,55 @@
 //  Created by Wen Chien Chen on 2021/1/17.
 //
 
-import Foundation
+import XCTest
+import FBSnapshotTestCase
+@testable import CurrencyConversion
+
+final class ExchangeRateCellSnapshotTests: FBSnapshotTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        self.recordMode = false
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testNilDefault() {
+        let cell = UINib(nibName: "ExchangeRateCell", bundle: nil).instantiate(withOwner: nil,
+                                                                           options: nil)[0] as! ExchangeRateCell
+        cell.configCell(cellModel: nil)
+        
+        verifyView(cell)
+    }
+    
+    func testDefault() {
+        let TWDExchangeRateModel = ExchangeRateCellModel(fromCurrencyName: "USD",
+                                                         toCurrencyFullName: "Taiwan Dollar",
+                                                         toCurrencyShortName: "TWD",
+                                                         rate: 28.1,
+                                                         amount: 2810)
+        let cell = UINib(nibName: "ExchangeRateCell", bundle: nil).instantiate(withOwner: nil,
+                                                                           options: nil)[0] as! ExchangeRateCell
+        cell.configCell(cellModel: TWDExchangeRateModel)
+        
+        verifyView(cell)
+    }
+}
+
+extension FBSnapshotTestCase {
+    
+    func verifyView(_ cell: UICollectionViewCell) {
+        cell.contentView.setNeedsUpdateConstraints()
+        cell.contentView.updateConstraintsIfNeeded()
+        
+        cell.contentView.setNeedsLayout()
+        cell.contentView.layoutIfNeeded()
+        
+        cell.frame.size = CGSize(width: 117, height: 100)
+        FBSnapshotVerifyView(cell)
+    }
+    
+}
+
